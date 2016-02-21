59d9a4a33a3e40f963dbe5b64f5cba447e9c578c
Adam Marcus <marcua@marcua.net>
2016-02-21 17:50:01 -0500

    Some changes to histogram

--- fortune500/year_histogram.py
+++ fortune500/year_histogram.py
@@ -9,7 +9,10 @@ counts = df.groupby('company')['rank'].agg({'num_times':len}).reset_index()
 histogram = counts.groupby('num_times')['company'].agg({'total': len, 'companies': lambda x: list(x)})
 
 recs = histogram.to_records()
-recs = [dict(zip(('total', 'companies', 'count'), (int(r[0]), r[1], int(r[2])))) for r in recs]
+recs = {
+    'data': [dict(zip(('x', 'y'), (int(r[0]), int(r[2])))) for r in recs],
+    'companies': dict((int(r[0]), r[1]) for r in recs),
+}
 output = json.dumps(recs)
 
 outf = open(sys.argv[2], "w")
